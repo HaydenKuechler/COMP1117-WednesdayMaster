4faca519d0003ce9159c2a05241001a32fd739be
HaydenKuechler <haydenkue.business@gmail.com>
2026-03-26 16:32:28 -0400

    Followedalogtovideo

--- Assets/Scripts/UI/Cherry.cs
+++ Assets/Scripts/UI/Cherry.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.Events;
 
-public class Cherry : MonoBehaviour
+/* public class Cherry : MonoBehaviour
 {
   public UnityEvent onCollected = new UnityEvent();
 
@@ -25,3 +25,4 @@ public class Cherry : MonoBehaviour
     }
 }
 
+*/
